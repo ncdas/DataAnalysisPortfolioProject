867135575ea9515e37068e6e5e406380f15f3aaa
NC Das <nimai.iyf@gmail.com>
2023-03-11 02:26:11 +0600

    Breaking out Address into Individual Columns (Address, City, State)

--- SQL/DataCleaning_housingData.sql
+++ SQL/DataCleaning_housingData.sql
@@ -7,8 +7,98 @@ FROM [CovidProject].[dbo].[NashvilleHousingData ]
 SELECT SaleDate, CONVERT(date,Saledate)
 FROM [CovidProject].[dbo].[NashvilleHousingData ]
 
-Update [CovidProject].[dbo].[NashvilleHousingData ]
-SET SaleDate = CONVERT(date,Saledate)
+ALTER TABLE [CovidProject].[dbo].[NashvilleHousingData ]
+ADD Sale_Date date;
 
-SELECT SaleDate
+
+UPDATE [CovidProject].[dbo].[NashvilleHousingData ]
+SET Sale_Date = CONVERT(date,Saledate)
+
+SELECT Sale_Date
+FROM [CovidProject].[dbo].[NashvilleHousingData ]
+
+ALTER TABLE [CovidProject].[dbo].[NashvilleHousingData ]
+DROP COLUMN SaleDate; 
+
+-- Populate Property Address data(Dealing with NULL)
+
+
+SELECT PropertyAddress
+FROM [CovidProject].[dbo].[NashvilleHousingData ]
+WHERE PropertyAddress is null
+
+SELECT T.ParcelID, T.PropertyAddress, Ta.ParcelID, Ta.PropertyAddress, ISNULL(T.PropertyAddress, Ta.PropertyAddress)
+FROM [CovidProject].[dbo].[NashvilleHousingData ] T
+JOIN [CovidProject].[dbo].[NashvilleHousingData ] Ta
+	ON T.ParcelID = Ta.ParcelID
+	And T.[UniqueID ] <> Ta.[UniqueID ]
+WHERE T.PropertyAddress is null
+
+Update T
+SET PropertyAddress = ISNULL(T.PropertyAddress, Ta.PropertyAddress)
+FROM [CovidProject].[dbo].[NashvilleHousingData ] T
+JOIN [CovidProject].[dbo].[NashvilleHousingData ] Ta
+	ON T.ParcelID = Ta.ParcelID
+	And T.[UniqueID ] <> Ta.[UniqueID ]
+--WHERE T.PropertyAddress is null
+
+SELECT *
+FROM [CovidProject].[dbo].[NashvilleHousingData ]
+WHERE PropertyAddress is null
+
+
+
+-- Breaking out Address into Individual Columns (Address, City, State)
+
+SELECT PropertyAddress
+FROM [CovidProject].[dbo].[NashvilleHousingData ]
+
+--Lets create columns to store address and city
+
+ALTER TABLE [CovidProject].[dbo].[NashvilleHousingData ]
+ADD Property_Address nvarchar(255)
+
+ALTER TABLE [CovidProject].[dbo].[NashvilleHousingData ]
+ADD Property_City nvarchar(255)
+
+--Breaking out address and city
+SELECT 
+SUBSTRING(PropertyAddress, 1, CHARINDEX(',' , PropertyAddress) -1) as a
+, SUBSTRING(PropertyAddress, CHARINDEX(',' , PropertyAddress) +1, LEN(PropertyAddress)) as b
+FROM [CovidProject].[dbo].[NashvilleHousingData ]
+
+Update [CovidProject].[dbo].[NashvilleHousingData ]
+SET Property_Address = SUBSTRING(PropertyAddress, 1, CHARINDEX(',' , PropertyAddress) -1)
+
+Update [CovidProject].[dbo].[NashvilleHousingData ]
+SET Property_City = SUBSTRING(PropertyAddress, CHARINDEX(',' , PropertyAddress) +1, LEN(PropertyAddress))
+
+
+--Split address, city, state
+
+
+SELECT OwnerAddress
+FROM [CovidProject].[dbo].[NashvilleHousingData ]
+
+
+SELECT 
+PARSENAME(REPLACE(OwnerAddress, ', ' , '.') ,3), 
+PARSENAME(REPLACE(OwnerAddress, ', ' , '.') ,2), 
+PARSENAME(REPLACE(OwnerAddress, ', ' , '.') ,1)
+FROM [CovidProject].[dbo].[NashvilleHousingData ]
+
+
+ALTER TABLE [CovidProject].[dbo].[NashvilleHousingData ]
+ADD Owner_Address nvarchar(255), 
+Owner_City nvarchar(255), 
+Owner_State nvarchar(255)
+
+
+
+Update [CovidProject].[dbo].[NashvilleHousingData ]
+SET Owner_Address = PARSENAME(REPLACE(OwnerAddress, ', ' , '.') ,3), 
+Owner_City = PARSENAME(REPLACE(OwnerAddress, ', ' , '.') ,2), 
+Owner_State = PARSENAME(REPLACE(OwnerAddress, ', ' , '.') ,1)
+
+SELECT *
 FROM [CovidProject].[dbo].[NashvilleHousingData ]
